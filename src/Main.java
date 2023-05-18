242ab9c85d99a47c5159efb252116862b3b89dc8
MysteriousFoxArt <72874807+MysteriousFoxArt@users.noreply.github.com>
2023-05-18 19:42:18 +0300

    0.1с

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
+
+    //Вывод текста и новвая строка
+    public static void PrnLn(String words){
+        System.out.println(words);
+    }
+    //Вывод теста и без крастной строки
+    public static void Prn(String words){
+        System.out.print(words);
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc_In = new Scanner(System.in);
+        Prn("Введите ваше имя: ");
+        String name_Name = sc_In.nextLine();
+
+        Prn("Привет ");
+        Prn(name_Name);
     }
 }
